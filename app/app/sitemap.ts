10d0a5056d22d63e3ebf81027505b07f964c4986
unknown <almontimanuputty@gmail.com>
2026-03-19 09:47:46 +0700

    adding the sitemap modifications

--- app/app/sitemap.ts
+++ app/app/sitemap.ts
@@ -3,16 +3,10 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://almontijourdanm.com',
+      url: 'https://almontijourdanm.com/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
-    },
-    {
-      url: 'https://portofolio.almontijourdanm.com',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
     },
   ]
 }
